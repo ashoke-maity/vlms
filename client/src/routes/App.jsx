0f3bf357e7f555e3eede0ddb22d44bbcb37e456f
ashoke-maity <ashokemaity092@gmail.com>
2025-09-06 23:18:41 +0530

    modified:   client/src/components/ProtectedRoute.jsx 	modified:   client/src/components/ui/user/Header.jsx 	modified:   client/src/context/AuthContext.js 	new file:   client/src/context/AuthContext.jsx 	new file:   client/src/pages/user/Browse.jsx 	modified:   client/src/pages/user/Login.jsx 	new file:   client/src/pages/user/MyList.jsx 	modified:   client/src/pages/user/Profile.jsx 	modified:   client/src/pages/user/Register.jsx 	modified:   client/src/pages/user/Settings.jsx 	modified:   client/src/routes/App.jsx

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from '../context/AuthContext';
+import { AuthProvider } from '../context/AuthContext.jsx';
 import Home from '../pages/user/Home';
 import Login from '../pages/user/Login';
 import Register from '../pages/user/Register';
